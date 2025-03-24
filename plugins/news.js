5ad4c7a49b8183bd93b776411167df90c8ae7855
Stanley Masanyiwa <priscajohnson976@gmail.com>
2025-03-24 03:38:24 +0300

    Update news.js

--- plugins/news.js
+++ plugins/news.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, reply }) => {
 ⚠️ _${article.description}_
 🔗 _${article.url}_
 
-  ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ Mʀ Mᴜᴢᴀᴍɪʟ
+  ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ©𝚂𝚃𝙰𝙽𝚈-𝚃𝙴𝙲𝙷,™
             `;
 
             console.log('Article URL:', article.urlToImage); // Log image URL for debugging
