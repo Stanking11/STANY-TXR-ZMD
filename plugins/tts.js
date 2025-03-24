be925b5ab48979972315f26b8d93109b44867a5d
Stanley Masanyiwa <priscajohnson976@gmail.com>
2025-03-24 11:44:48 +0300

    Update tts.js

--- plugins/tts.js
+++ plugins/tts.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, q, reply }) => {
         const response = await axios.get(url);
         const translation = response.data.responseData.translatedText;
 
-        const translationMessage = `> *STANY-TECH TRX TRANSLATION*
+        const translationMessage = `> *STANY TXR ZMD-TRANSLATION*
 
 > 🔤 *Original*: ${textToTranslate}
 
