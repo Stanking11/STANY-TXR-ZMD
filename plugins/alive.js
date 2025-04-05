7806475b9d389b86bdef1a73e03a1f6a28413db8
Stanley Masanyiwa <priscajohnson976@gmail.com>
2025-04-06 02:12:31 +0300

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -26,7 +26,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
         // Send the status message with an image
         await conn.sendMessage(from, { 
-            image: { url: `https://files.catbox.moe/nu93in.jpg` },  // Image URL
+            image: { url: `https://files.catbox.moe/l3fp7g.jpg` },  // Image URL
             caption: status,
             contextInfo: {
                 mentionedJid: [m.sender],
