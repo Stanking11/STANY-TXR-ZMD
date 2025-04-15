16bb799385c0e69e5c49e3545648260db2473793
Stanley Masanyiwa <priscajohnson976@gmail.com>
2025-04-15 19:01:42 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -5,9 +5,9 @@ const { runtime } = require('../lib/functions');
 const axios = require('axios');
 
 cmd({
-    pattern: "menu",
+    pattern: "menu2",
     desc: "menu the bot",
-    category: "menu",
+    category: "allmenu",
     react: "🌸",
     filename: __filename
 }, 
