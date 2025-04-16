34e3db5c2ed9836efa727ecb22dbd4b9939330c7
Stanley Masanyiwa <priscajohnson976@gmail.com>
2025-04-16 09:55:02 +0300

    Update main.js

--- plugins/main.js
+++ plugins/main.js
@@ -5,7 +5,7 @@ const {runtime} = require('../lib/functions')
 const axios = require('axios')
 
 cmd({
-    pattern: "menu",
+    pattern: "menu1",
     alias: "menu",
     desc: "menu the bot",
     category: "menu",
