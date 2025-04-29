bf0840c5be2e1f759ae3e01c77c909c7845eefec
STANY-TECH <priscajohnson976@gmail.com>
2025-04-30 02:20:45 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || 'STANY_TXR',
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '255794481386',
+    PREFIX: process.env.PREFIX || '.',
+    ANTICALL: process.env.ANTICALL || '1',
+    ANTILINK: process.env.ANTILINK || '1',
+    // Add all other environment variables here
+};
