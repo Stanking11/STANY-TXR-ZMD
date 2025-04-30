56e82ae5e46fe9f74dfeada2f6b583e1504b7bd6
STANY-TECH <priscajohnson976@gmail.com>
2025-04-30 18:28:34 +0300

    Delete command.js

--- command.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    OWNER_NUMBER: '255750638502@s.whatsapp.net', // heelo made up by STANY-TECH 
-    BOT_NAME: 'Stany Tech',
-    PREFIX: '!',
-    ANTICALL: true,
-    ANTIDELETE: true,
-    ANTITAG: true,
-    ANTISTICKER: true
-};
