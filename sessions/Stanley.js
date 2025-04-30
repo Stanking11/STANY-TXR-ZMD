565f129ca87d0fe418074333d486eca1a514bdbc
STANY-TECH <priscajohnson976@gmail.com>
2025-04-30 18:15:37 +0300

    Delete sessions directory

--- sessions/Stanley.js
+++
@@ -1,7 +0,0 @@
-
-// Add this after auth initialization
-if (fs.existsSync('sessions/creds.json')) {
-    console.log('Using existing session...');
-} else {
-    console.log('New session - scan QR code');
-}
