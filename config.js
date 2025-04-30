e4337c3126f0577c378d05c92d6ba0707a24fab6
STANY-TECH <priscajohnson976@gmail.com>
2025-04-30 22:24:41 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: 'Stany Tech',
+  version: '2.0.1',
+  prefix: '#',
+  owner: ['255612285041'], // STANY-TXR-ZMD 
+  allowedGroups: [], // Add group IDs if needed
+  maxTagLimit: 5,
+  features: {
+    antiCall: true,
+    antiDelete: true,
+    antiTag: true,
+    antiSticker: true
+  }
+}
