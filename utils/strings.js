781e1f2d1982ccc4e4495a93139c8137d9bba879
STANY-TECH <priscajohnson976@gmail.com>
2025-05-04 15:32:59 +0300

    Delete utils directory

--- utils/strings.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  ANTI_SPAM: "⚠️ Please wait {cooldown} seconds before using another command!",
-  OWNER_ONLY: "❌ This command is only available for the bot owner!",
-  GROUP_ONLY: "❌ This command can only be used in groups!",
-  ADMIN_ONLY: "❌ This command requires admin privileges!",
-  ERROR_HEADER: "🚨 *Command Execution Failed*",
-  ERROR_FOOTER: "\nPlease try again later or contact support.",
-};
